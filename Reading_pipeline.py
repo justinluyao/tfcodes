2ddd617d8a5fd731ed3b5641aa3931e4cccfb535
sunnyluyao <sunnyluyao@vip.qq.com>
2017-10-24 15:47:49 +0100

    modified

--- Reading_pipeline.py
+++ Reading_pipeline.py
@@ -2,26 +2,23 @@
 
 import tensorflow as tf
 
-def read_batch_cpm(tfr_path, img_size, hmap_size, num_joints, center_radius, batch_size=16, num_epochs=None):
+def read_batch_cpm(tfr_path, img_size, hmap_size, input_channels, num_of_classes, batch_size=16, num_epochs=None):
 
     tfr_queue = tf.train.string_input_producer(tfr_path, num_epochs=num_epochs, shuffle=True)
 
 #multi_threads raeading
-    data_list = [read_and_decode_cpm(tfr_queue, img_size, num_joints, center_radius) for _ in range(2 * len(tfr_path))]
+    data_list = [read_and_decode_cpm(tfr_queue, img_size, input_channels, hmap_size, num_of_classes) for _ in range(2 * len(tfr_path))]
 
-    batch_images, batch_centers, batch_labels, batch_images_orig = tf.train.shuffle_batch_join(data_list,
-                                                                                               batch_size=batch_size,
-                                                                                               capacity=100 + 6 * batch_size,
-                                                                                               min_after_dequeue=100,
-                                                                                               enqueue_many=True,
-                                                                                               name='batch_data_read')
-
-    # batch_labels = tf.image.resize_bilinear(batch_labels, size=tf.constant((hmap_size,hmap_size), name='shape'))
+    batch_images, batch_labels, batch_images_orig = tf.train.shuffle_batch_join(data_list, batch_size=batch_size,
+                                                                                           capacity=100 + 6 * batch_size,
+                                                                                           min_after_dequeue=100,
+                                                                                           enqueue_many=True,
+                                                                                           name='batch_data_read')
 
     return batch_images, batch_centers, batch_labels, batch_images_orig
 
 
-def read_and_decode_cpm(tfr_queue, img_size, num_joints, center_radius):
+def read_and_decode_cpm(tfr_queue, img_size, hmap_size, input_channels, num_of_classes):
     tfr_reader = tf.TFRecordReader()
     _, serialized_example = tfr_reader.read(tfr_queue)
 
@@ -39,7 +36,7 @@ def read_and_decode_cpm(tfr_queue, img_size, num_joints, center_radius):
     # img_size = 128
     # center_radius = 11
     img = tf.decode_raw(features['image'], tf.uint8)
-    img = tf.reshape(img, [img_size, img_size, 3])
+    img = tf.reshape(img, [img_size, img_size, input_channels])
     img = tf.cast(img, tf.float32)
 
     # img = img[..., ::-1]
@@ -50,10 +47,14 @@ def read_and_decode_cpm(tfr_queue, img_size, num_joints, center_radius):
     # img = img[..., ::-1]
 
     # heatmap = tf.decode_raw(features['heatmaps'], tf.float32)
-    heatmap = tf.reshape(features['heatmaps'], [img_size, img_size, (num_of_classes)])
+    heatmap = tf.reshape(features['heatmaps'], [hmap_size, hmap_size, num_of_classes])
 
-    queue_images, queue_labels, queue_orig_images = preprocessing()
-
+    queue_images, queue_labels, queue_orig_images = preprocessing(img, heatmap)
 
     return queue_images, queue_labels, queue_orig_images
     # return preprocessed_img, preprocessed_center_maps, preprocessed_heatmaps, img
+
+def pre_processing(img, heatmap):
+
+
+    return img, heatmaps, img
--- Train.py
+++ Train.py
@@ -22,6 +22,7 @@ import depth_model
 # import the pre_processing file
 import pre_processing as prep
 
+import Reading_pipeline as rp
 # training on the server
 if cfg.FLAGS.isbc is False:
     import cv2
@@ -30,26 +31,20 @@ if cfg.FLAGS.isbc is False:
 if __name__ == '__main__':
 
     ######Pre-processing====================================================================================
-    batch_x, batch_y, batch_x_orig = prep.read_batch_cpm(cfg.FLAGS.TFrecords_dir, FLAGS.tfr_data_files, FLAGS.input_size,
-                                                                      FLAGS.heatmap_size, FLAGS.num_of_joints,
-                                                                      FLAGS.center_radius, FLAGS.batch_size)
+    batch_x, batch_y, batch_x_orig = rp.read_batch_cpm(cfg.FLAGS.TFrecords_dir,
+                                                       cfg.FLAGS.input_size,
+                                                       cfg.FLAGS.heatmap_size,
+                                                       cfg.FLAGS.num_of_classes,
+                                                       cfg.FLAGS.input_channel,
+                                                       cfg.FLAGS.batch_size)
 
-    #    print('Reading Batch=====================================================================================')
-    #
-
-
-    depth_batch, hmap_batch = tf.train.shuffle_batch([depth, hmap],
-                                                     batch_size=cfg.FLAGS.batch_size, capacity=10,
-                                                     min_after_dequeue=5)
-
-    ##
     ##    #
     ##    ##make input place holder========================================================================
     input_placeholder = tf.placeholder(dtype=tf.float32,
-                                       shape=(cfg.FLAGS.batch_size, cfg.FLAGS.input_height, cfg.FLAGS.input_width, 3),
+                                       shape=(cfg.FLAGS.batch_size, cfg.FLAGS.input_height, cfg.FLAGS.input_width, cfg.FLAGS.input_channel),
                                        name='input_placeholer')
 
-    seg_placeholder = tf.placeholder(dtype=tf.float32,
+    label_placeholder = tf.placeholder(dtype=tf.float32,
                                      shape=(cfg.FLAGS.batch_size, cfg.FLAGS.hmap_height, cfg.FLAGS.hmap_width,
                                             cfg.FLAGS.num_of_classes), name='hmap_placeholder')
     ##    #
@@ -70,7 +65,7 @@ if __name__ == '__main__':
         tf_w = tf.summary.FileWriter('./log', sess.graph)
 
         # Create model saver
-        saver = tf.train.Saver(max_to_keep=1)
+        saver = tf.train.Saver(max_to_keep=2)
 
         sess.run(tf.global_variables_initializer())
 
@@ -79,85 +74,16 @@ if __name__ == '__main__':
         while True:
             # Read in batch data
             batch_x_np, batch_y_np = sess.run([depth_batch, hmap_batch])
-            #            print(np.sum(batch_x_np))
-            #            print(np.argwhere(np.isnan(batch_y_np)))
-            batch_y_np = np.nan_to_num(batch_y_np)
 
             stage_losses_np, total_loss_np, summary, current_lr, stage_heatmap_np, global_step = model.model_train(
                 batch_x_np, batch_y_np, sess)
-            #            print(np.shape(batch_y_np))
-            #            print(np.shape(stage_heatmap_np[0]))
-            #            ss = np.sum(stage_heatmap_np[-1],axis=-1)
-            #            ss = np.sum(ss,axis=-2)
-            #            ss = np.sum(ss)
-            #            print(ss)
-            #            print(np.max(stage_heatmap_np) )
-            #            print(np.min(stage_heatmap_np) )
 
             # Write logs
             tf_w.add_summary(summary, global_step)
             # Draw intermediate results
-            #             Draw intermediate results
-            if global_step % 1 == 0:
-                #
-                demo_img = batch_x_np[0] + 0.5
-                demo_stage_heatmaps = []
-                for stage in range(cfg.FLAGS.stages):
-                    print()
-                    demo_stage_heatmap = stage_heatmap_np[stage][0, :, :, 0:cfg.FLAGS.num_of_joints].reshape(
-                        (cfg.FLAGS.hmap_height, cfg.FLAGS.hmap_width, cfg.FLAGS.num_of_joints))
-                    demo_stage_heatmap = cv2.resize(demo_stage_heatmap, (cfg.FLAGS.input_height, cfg.FLAGS.input_width))
-                    demo_stage_heatmap = np.amax(demo_stage_heatmap, axis=2)
-                    demo_stage_heatmap = np.reshape(demo_stage_heatmap,
-                                                    (cfg.FLAGS.input_height, cfg.FLAGS.input_width, 1))
-                    #                   demo_stage_heatmap = np.repeat(demo_stage_heatmap, 3, axis=2)
-                    demo_stage_heatmaps.append(demo_stage_heatmap)
 
-                demo_gt_heatmap = batch_y_np[0, :, :, 0:cfg.FLAGS.num_of_joints].reshape(
-                    cfg.FLAGS.hmap_height, cfg.FLAGS.hmap_width, cfg.FLAGS.num_of_joints)
-                demo_gt_heatmap = cv2.resize(demo_gt_heatmap, (cfg.FLAGS.input_height, cfg.FLAGS.input_width))
-                demo_gt_heatmap = np.amax(demo_gt_heatmap, axis=2)
-                demo_gt_heatmap = np.reshape(demo_gt_heatmap, (cfg.FLAGS.input_height, cfg.FLAGS.input_width, 1))
-                #                demo_gt_heatmap = np.repeat(demo_gt_heatmap, 3, axis=2)
-
-                if cfg.FLAGS.stages > 4:
-                    #                   upper_img = np.concatenate((demo_stage_heatmaps[0], demo_stage_heatmaps[1], demo_stage_heatmaps[2]),
-                    #                                              axis=1)
-                    blend_img = 0.5 * demo_gt_heatmap + 0.5 * demo_img
-                    demo_img = cv2.cvtColor(demo_img, cv2.COLOR_RGB2GRAY)
-
-                    #                   blend_img = np.reshape(blend_img, (1,cfg.FLAGS.input_height, cfg.FLAGS.input_width, 1))
-
-                    print(np.shape(blend_img))
-                    #                   blend_img=blend_img[:,:,0]
-                    lower_img = np.concatenate((demo_stage_heatmaps[cfg.FLAGS.stages - 1], demo_gt_heatmap),
-                                               axis=1)
-                    #                   demo_img = np.concatenate((blend_img*255, lower_img), axis=0)
-                    cv2.imshow('demo', (demo_img * 255).astype(np.uint8))
-                    cv2.waitKey(100)
-                else:
-                    upper_img = np.concatenate((cfg.FLAGS.stages - 1, demo_gt_heatmap, demo_img),
-                                               axis=0)
-
-                    #                 print(np.shape(upper_img))
-                    #                 cv2.imshow('current heatmap', (demo_img * 255).astype(np.uint8))
-                    #                 cv2.waitKey(100)
-
-            ##
-            print('##========Iter {:>6d}========##'.format(global_step))
-            print('Current learning rate: {:.8f}'.format(current_lr))
-            for stage_num in range(cfg.FLAGS.stages):
-                print('Stage {} loss: {:>.3f}'.format(stage_num + 1, stage_losses_np[stage_num]))
-            print('Total loss: {:>.3f}\n\n'.format(total_loss_np))
-
-            #            # Save models
             model.save_model(global_step, saver, sess)
-            #	         if global_step % 10 == 1:
-            #	           with tf.device('/cpu:0'):
-            ##                  save_path_str = os.path.join('D:/Projects/TFtraining/saver',str(global_step)+'.ckpt')
-            #	             saver.save(sess=sess, save_path="/saver/model.ckpt",global_step=global_step)
-            ##                  saver.save()
-            #	             print('\nModel checkpoint saved...\n')
+
 
             # Finish training
             print(global_step)
@@ -168,3 +94,57 @@ if __name__ == '__main__':
         coord.join(threads)
         ##
         print('Training done.')
+
+def visualizing_training(global_step, stage_heatmap_np):
+    if global_step % 1 == 0:
+        #
+        demo_img = batch_x_np[0] + 0.5
+        demo_stage_heatmaps = []
+        for stage in range(cfg.FLAGS.stages):
+            print()
+            demo_stage_heatmap = stage_heatmap_np[stage][0, :, :, 0:cfg.FLAGS.num_of_joints].reshape(
+                (cfg.FLAGS.hmap_height, cfg.FLAGS.hmap_width, cfg.FLAGS.num_of_joints))
+            demo_stage_heatmap = cv2.resize(demo_stage_heatmap, (cfg.FLAGS.input_height, cfg.FLAGS.input_width))
+            demo_stage_heatmap = np.amax(demo_stage_heatmap, axis=2)
+            demo_stage_heatmap = np.reshape(demo_stage_heatmap,
+                                            (cfg.FLAGS.input_height, cfg.FLAGS.input_width, 1))
+            #                   demo_stage_heatmap = np.repeat(demo_stage_heatmap, 3, axis=2)
+            demo_stage_heatmaps.append(demo_stage_heatmap)
+
+        demo_gt_heatmap = batch_y_np[0, :, :, 0:cfg.FLAGS.num_of_joints].reshape(
+            cfg.FLAGS.hmap_height, cfg.FLAGS.hmap_width, cfg.FLAGS.num_of_joints)
+        demo_gt_heatmap = cv2.resize(demo_gt_heatmap, (cfg.FLAGS.input_height, cfg.FLAGS.input_width))
+        demo_gt_heatmap = np.amax(demo_gt_heatmap, axis=2)
+        demo_gt_heatmap = np.reshape(demo_gt_heatmap, (cfg.FLAGS.input_height, cfg.FLAGS.input_width, 1))
+        #                demo_gt_heatmap = np.repeat(demo_gt_heatmap, 3, axis=2)
+
+        if cfg.FLAGS.stages > 4:
+            #                   upper_img = np.concatenate((demo_stage_heatmaps[0], demo_stage_heatmaps[1], demo_stage_heatmaps[2]),
+            #                                              axis=1)
+            blend_img = 0.5 * demo_gt_heatmap + 0.5 * demo_img
+            demo_img = cv2.cvtColor(demo_img, cv2.COLOR_RGB2GRAY)
+
+            #                   blend_img = np.reshape(blend_img, (1,cfg.FLAGS.input_height, cfg.FLAGS.input_width, 1))
+
+            print(np.shape(blend_img))
+            #                   blend_img=blend_img[:,:,0]
+            lower_img = np.concatenate((demo_stage_heatmaps[cfg.FLAGS.stages - 1], demo_gt_heatmap),
+                                       axis=1)
+            #                   demo_img = np.concatenate((blend_img*255, lower_img), axis=0)
+            cv2.imshow('demo', (demo_img * 255).astype(np.uint8))
+            cv2.waitKey(100)
+        else:
+            upper_img = np.concatenate((cfg.FLAGS.stages - 1, demo_gt_heatmap, demo_img),
+                                       axis=0)
+
+            #                 print(np.shape(upper_img))
+            #                 cv2.imshow('current heatmap', (demo_img * 255).astype(np.uint8))
+            #                 cv2.waitKey(100)
+
+    ##
+    print('##========Iter {:>6d}========##'.format(global_step))
+    print('Current learning rate: {:.8f}'.format(current_lr))
+    for stage_num in range(cfg.FLAGS.stages):
+        print('Stage {} loss: {:>.3f}'.format(stage_num + 1, stage_losses_np[stage_num]))
+    print('Total loss: {:>.3f}\n\n'.format(total_loss_np))
+
